8f828d9d3921e085c0794e71115983754a57eac3
Jon Ah-Nee <jonahnee@gmail.com>
2016-01-31 23:41:18 -1000

    completed tutorial

--- server.js
+++ server.js
@@ -6,6 +6,25 @@ var PORT = 3000;
 
 var app = express();
 
+// middleware
+var user = { username : 'bob', password : 'secret', email : 'bob@example.com'};
+passport.use(new BasicStrategy(
+  function (username, password, done) {
+    // example authentication strategy using
+    if ( !(username === user.username && password === user.password) ) {
+      return done(null, false);
+    }
+    return done(null, user);
+  }
+));
+
+// routes
+app.get('/secret',
+  passport.authenticate('basic', { session : false}),
+  function (req, res) {
+    res.json(req.user);
+  });
+
 var server = app.listen(PORT, function () {
   console.log('Server listening on port', PORT);
 });
